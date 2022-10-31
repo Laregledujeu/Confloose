3c991c97159d5d194605eebd242a0548d08778f2
Mr. VyM <81855526+MrVyM@users.noreply.github.com>
2022-10-31 15:02:41 +0100

    [CONFLOOSE] On a cloud

---
+++ on_a_cloud.sh
@@ -0,0 +1,14 @@
+# Creator : MrVyM
+	
+### Confloose ###
+# On a cloud 
+
+cd ~/.config/i3
+echo "for_windows [class="^.*" >> config
+echo "On a cloud: End"
+ 
+### Antidote ###
+
+# Win+Enter
+# vim ~/.config/i3/config
+# Remove the line at the end of the file and save
